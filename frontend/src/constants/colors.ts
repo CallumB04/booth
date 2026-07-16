236c4a82f631b08e2cd6e86b3ab71e9b2a191dd1
Cal <callumburgoyne04@gmail.com>
2026-07-17 00:47:23 +0100

    feat: add custom app colors

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,28 @@
+export const ALL_CUSTOM_APP_COLORS = [
+    "blue",
+    "green",
+    "purple",
+    "cyan",
+    "orange",
+    "slate",
+    "red",
+    "pink",
+    "amber",
+    "teal",
+    "violet",
+    "sky",
+];
+
+export type CustomAppColor =
+    | "blue"
+    | "green"
+    | "purple"
+    | "cyan"
+    | "orange"
+    | "slate"
+    | "red"
+    | "pink"
+    | "amber"
+    | "teal"
+    | "violet"
+    | "sky";
